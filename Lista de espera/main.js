74c49e0cad1b1bdab45f9cb2a3c99519370f4b9a
diegoed1 <96401679+diegoed1@users.noreply.github.com>
2022-02-15 19:31:58 -0300

    origin

---
+++ Lista de espera/main.js
@@ -0,0 +1,40 @@
+//função para preencher os campos do formulario
+const completarFormulario = (endereco) =>{
+ document.getElementById('endereco').value = endereco.logradouro;
+ document.getElementById('bairro').value = endereco.bairro;
+ document.getElementById('cidade').value = endereco.localidade;
+ document.getElementById('estado').value = endereco.uf;
+};
+
+//função para pesquisar o cep pela url da API
+    const pesquisarCep = async() => {
+    const cep = document.getElementById('cep').value;
+
+    //aplicado API em json
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+   
+    completarFormulario(endereco);
+    console.log(endereco)
+}
+
+//evento no campo, quando preenche o cep e tenta ir para o proximo campo (focusout), ira executar a função pesquisarCep e completarFormulario
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+
+
+//ocultação dos campo de endereço
+document.getElementById('cep').addEventListener('focusout', function(){
+    let endereco = document.querySelectorAll('.endereco');
+ for(let i=0; i<endereco.length;i++) {
+     if(endereco[i].style.display ==="none") {
+    endereco[i].style.display ="block";
+} else{
+    endereco[i].style.display = "none";
+    }
+    console.log(endereco[i].style.display)
+    endereco[i].style.display = 'block'
+ }
+})
+
